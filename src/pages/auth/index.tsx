00f53a9b18d4b8e5ccd2b2a35816d774ae004aee
Chugunovvvv <cugunovroman03@gmail.com>
2024-08-02 20:05:35 +0300

    dev: add tabs for auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,7 +1,27 @@
-import React from "react"
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
+import React, { useState } from "react"
 
 const Auth = () => {
-  return <div>Auth</div>
+  const [selected, setSelected] = useState<string>("login")
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={key => setSelected(key as string)}
+            >
+              <Tab key="login" title="Вход"></Tab>
+              <Tab key="register" title="Регистрация"></Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default Auth
